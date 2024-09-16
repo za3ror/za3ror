2358fbbc5cc7880868af89a01d6cd841c0135407
za3ror <mdmenjoyer@gmail.com>
2024-09-16 14:25:40 +0300

    Update za3rour-main.js

---
+++ za3rour-main.js
@@ -0,0 +1,41 @@
+let backupButton = document.getElementById('backup');
+let contentDiv = document.getElementById('content-div');
+
+document.body.onscroll = function(){
+if(scrollY >= 400){
+    backupButton.style.display = 'block';
+} else{
+    backupButton.style.display = 'none';
+};
+
+};
+
+backupButton.onclick = function() {
+    window.scroll({
+        left:0,
+        top:0,
+        behavior:'smooth'
+    })
+};
+
+
+function createContent(count){
+
+
+    let content = '';
+
+    for(let i = 0; i < count; i++){
+
+    content += 
+    `   <div id="image">
+            <img src="Buy_Plants_Online_Cheap_Prices.png" alt=":/" class="content-image">
+            <br>
+            <h3 style="padding:0 70px;">العنوان</h3>
+        </div>`
+
+    contentDiv.innerHTML = content;
+
+    };
+};
+
+createContent(15)
